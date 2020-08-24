7d0d43bdedae2de8beac2ac9e0b55cddbbd82cc4
Kristian <kn@unisport.dk>
2020-08-24 10:46:12 +0200

    Adding ranom string builder to fake order_id

--- subscriptionservice/SubscriptionService.Tests/SubscriptionTest.cs
+++ subscriptionservice/SubscriptionService.Tests/SubscriptionTest.cs
@@ -3,6 +3,7 @@ using Xunit;
 using Subscription.Services;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Text;
 
 namespace SubscriptionServiceTests
 {
@@ -25,8 +26,20 @@ namespace SubscriptionServiceTests
         [Fact]
         public async Task CreateNewSubscriptionTest()
         {
-            int subscriptionId = await sut.Create("ladida5", "DKK", "Kitty porn subscription");
+            int subscriptionId = await sut.Create(RandomString(10), "DKK", "Kitty porn subscription");
             Assert.True(subscriptionId > 0);
+        }
+
+        private static string RandomString(int length)
+        {
+          const string pool = "abcdefghijklmnopqrstuvwxyz0123456789";
+          Random random = new Random();
+          StringBuilder builder = new StringBuilder();
+          for (int i = 0; i < length; i++) {
+            var c = pool[random.Next(0, pool.Length)];
+            builder.Append(c);
+          }
+          return builder.ToString();
         }
     }
 }
